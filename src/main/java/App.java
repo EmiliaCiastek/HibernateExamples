278b870a2725a623eaa9957c0bdd869f45e41a8c
Emilia <emciastek@gmail.com>
2018-02-02 13:00:07 +0100

    Adding element to DB

---
+++ src/main/java/App.java
@@ -0,0 +1,76 @@
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+
+public class App {
+  private static SessionFactory sessionFactory;
+
+  private static void configureSessionFactory() throws HibernateException {
+    sessionFactory = new Configuration().configure().buildSessionFactory();
+  }
+
+  public static void main(String[] args) {
+    // Configure the session factory
+    configureSessionFactory();
+
+    List<Employee> employees = new ArrayList<>();
+    Employee firstEmployee = new Employee("First", 10000.0);
+    Employee secondEmployee = new Employee("Other", 20000.0);
+    employees.add(firstEmployee);
+    employees.add(secondEmployee);
+
+    putDataToDataBase(employees);
+
+    System.out.println("Added");
+
+  }
+
+  private static void putDataToDataBase(List<Employee> employees) {
+    Session session = null;
+    Transaction tx = null;
+
+    try {
+      session = sessionFactory.openSession();
+      tx = session.beginTransaction();
+
+      employees.forEach(session::save);
+
+      // Committing the change in the database.
+      session.flush();
+      tx.commit();
+      session.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+
+      // Rolling back the changes to make the data consistent in case of any failure
+      // in between multiple database write operations.
+      tx.rollback();
+    } finally {
+      if (session != null) {
+        session.close();
+      }
+    }
+  }
+
+  private static Employee getById(int Id) {
+    Session session = null;
+    Transaction tx = null;
+
+
+      session = sessionFactory.openSession();
+      tx = session.beginTransaction();
+
+
+    }
+  }
+}
---
+++ src/main/java/Employee.java
@@ -0,0 +1,68 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  @Column(name = "firstName")
+  private String firstName;
+
+  @Column(name = "salary")
+  private double salary;
+
+//  @JoinColumn(name = "department")
+//  @ManyToOne
+//  private Department department;
+
+  public Employee() {
+  }
+
+  public Employee(String firstName, double salary) {
+    this.firstName = firstName;
+    this.salary = salary;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(double salary) {
+    this.salary = salary;
+  }
+
+//  public Department getDepartment() {
+//    return department;
+//  }
+//
+//  public void setDepartment(Department department) {
+//    this.department = department;
+//  }
+}
